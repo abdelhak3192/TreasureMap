ac60be52414e8b276ff2639f56037a923314dab3
Abdelhak BENMAZOUZA <abdelhak.benmazouza@modiscloud.net>
2023-10-22 20:15:06 +0200

    Add services

--- TreasureMap/Program.cs
+++ TreasureMap/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using TreasureMap.Interfaces;
+using TreasureMap.ObjectCreators;
+using TreasureMap.Services;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddSingleton<IImportDataService, ImportDataService>();
+builder.Services.AddScoped<IMovementCreator, SimpleAdvenceMovementCreator>();
+builder.Services.AddScoped<IMovementCreator, SimpleRightMovementCreator>();
+builder.Services.AddScoped<IMovementCreator, SimpleLeftMovementCreator>();
+builder.Services.AddSingleton<ITreasureMapService,TreasureMapService >();
+
+IHost host = builder.Build();
+host.Services.GetRequiredService<ITreasureMapService>().Play();
+await host.RunAsync();
